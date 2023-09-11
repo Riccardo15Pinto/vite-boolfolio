4f91ed99ac8054cef9b183d807e4408c1894d568
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-11 16:08:14 +0200

    create AppCard and pass project by props

---
+++ src/partials/AppCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        project: 'Object',
+    }
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.name_project }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">{{ project.slug_project }}</h6>
+            <p class="card-text">{{ project.description_project }}</p>
+            <a :href="project.url_project" class="card-link">Vedi Repo su GitHub</a>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>

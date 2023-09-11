c15c6fb6d501cb615f94acbe861d2db98ac97f57
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-11 16:08:46 +0200

    create main import appCard and pass array in appcard by props

---
+++ src/components/AppMain.vue
@@ -0,0 +1,30 @@
+<script>
+import AppCard from '../partials/AppCard.vue';
+
+export default {
+    components: { AppCard },
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        projects: 'Array',
+    },
+
+}
+
+</script>
+
+<template>
+    <div class="container my-3">
+
+        <div class="row row-cols-3">
+            <div class="col" v-for="project in projects">
+                <AppCard :project="project" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>

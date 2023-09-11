453dfd2a577ffa10e72d63cd6afb4e451b1bdc6c
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-11 16:15:50 +0200

    add project slug in appcard

--- src/partials/AppCard.vue
+++ src/partials/AppCard.vue
@@ -16,7 +16,7 @@ export default {
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">{{ project.name_project }}</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">{{ project.slug_project }}</h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary">{{ project.slug }}</h6>
             <p class="card-text">{{ project.description_project }}</p>
             <a :href="project.url_project" class="card-link">Vedi Repo su GitHub</a>
         </div>

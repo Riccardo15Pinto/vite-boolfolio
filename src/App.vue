32c0060e1565639eabcd1374a78fc65e14fb730f
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-11 15:19:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from '../node_modules/axios';
+let endpoint = 'http://127.0.0.1:8000/api/projects';
+export default {
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  methods: {
+
+    getProjectList() {
+      axios.get(endpoint).then(res => {
+        this.projects = res.data;
+      })
+    },
+
+  },
+
+  created() {
+
+    this.getProjectList()
+  }
+}
+
+</script>
+
+<template>
+  <h1 class="text-primary">ciao</h1>
+
+  <ul>
+    <li v-for="project in projects" :key="project.id">{{ project.name_project }}</li>
+  </ul>
+</template>
+
+<style scoped></style>

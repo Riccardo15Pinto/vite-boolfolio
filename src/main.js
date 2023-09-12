090c2b95d80e41b10820bf395f6628be04fc00b5
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-12 18:31:44 +0200

    fix import and use router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import  'bootstrap/dist/css/bootstrap.min.css'
-import {router} from './router'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import  'bootstrap/dist/css/bootstrap.min.css';
+import {router} from './router';
 
-
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app')

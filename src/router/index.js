20a8f1b91af2d5c889b07585b93278d191dd82c4
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-12 18:31:54 +0200

    add route detailpage

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import {createRouter , createWebHistory} from 'vue-router';
 
 import HomePage from '../pages/HomPage.vue';
+import DetailPage from '../pages/DetailPage.vue';
 
 const router = createRouter({
     history:createWebHistory(),
     linkExactActiveClass: 'active',
     routes:[
         { path: '/' , name: 'home' , component: HomePage},
+        { path: '/projects/:id' , name: 'DetailPage' , component: DetailPage},
     ]
 });
 

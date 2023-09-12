12b2da2a34d1439093ff2c40b91d4e235cc6dd58
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-12 17:25:27 +0200

    import homepage

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,20 @@
 <script>
-import axios from '../node_modules/axios';
-let endpoint = 'http://127.0.0.1:8000/api/projects';
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
-import AppPagination from './components/AppPagination.Vue';
+import HomPage from './pages/HomPage.vue';
+
 export default {
-  components: { AppHeader, AppMain, AppPagination },
+  components: { AppHeader, HomPage },
   data() {
     return {
-      projects: [],
-      links: [],
     }
   },
-  methods: {
 
-    getProjectList(target = 'http://127.0.0.1:8000/api/projects') {
-
-      // console.log(target)
-      axios.get(target).then(res => {
-        this.projects = res.data.data;
-        this.links = res.data.links;
-      })
-    },
-
-  },
-
-  created() {
-
-    this.getProjectList()
-  }
 }
-
 </script>
 
 <template>
   <AppHeader />
-  <AppMain :projects="this.projects" />
-  <AppPagination :links="this.links" @currentPage="getProjectList" />
+  <HomPage />
 </template>
 
 <style scoped></style>

ecb64fc0cc1d7ce3122167d8265dc4250360e29d
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-13 17:20:35 +0200

    add validation frontend

--- src/pages/FormPage.vue
+++ src/pages/FormPage.vue
@@ -1,31 +1,57 @@
 <script>
 import axios from 'axios';
 const endpoint = 'http://127.0.0.1:8000/api/contacts';
-const formField = { email: null, object: null, content: null };
+const formField = { email: '', object: '', content: '' };
 export default {
     data() {
         return {
             form: { ...formField },
+            errors: {},
+        }
+    },
+    computed: {
+        hasErrors() {
+            return Object.keys(this.errors).length;
         }
     },
     methods: {
 
         sendForm() {
 
+            this.validateForm();
 
+            if (this.hasErrors) return
             axios.post(endpoint, this.form)
                 .then(() => {
                     this.form = formField;
                 })
-                .catch(err => { console.error(err) })
+                .catch(err => {
+                    if (err.response.status === 400) {
+                        const { errors } = err.response.data;
+                        const errorMessage = {};
+                        for (let field in errors) errorMessage[field] = errors[field][0];
+                        this.errors = errorMessage;
+                        console.log(this.errors)
+                    }
+                })
                 .then(() => { })
         },
 
-    },
+        validateForm() {
 
-    created() {
-        console.log(this.form);
-    }
+            this.errors = {};
+            if (!this.form.email) {
+                this.errors.email = 'L\'E-mail è obbligatoria';
+            }
+            if (!this.form.object) {
+                this.errors.object = 'L\'Oggetto è obbligatoria';
+            }
+            if (!this.form.content) {
+                this.errors.content = 'Il Contenuto è obbligatoria';
+            }
+        }
+
+    },
 }
 
 </script>
@@ -35,17 +61,27 @@ export default {
         <form @submit.prevent="sendForm">
             <div class="mb-3">
                 <label for="email" class="form-label">Email:</label>
-                <input type="email" class="form-control" id="email" v-model.trim="form.email"
-                    placeholder="Inserisci l'email">
+                <input type="email" class="form-control" :class="{ 'is-invalid': errors.email }" id="email"
+                    v-model.trim="form.email" placeholder="Inserisci l'email">
+                <div v-if="errors.email" class="invalid-feedback">
+                    {{ errors.email }}
+                </div>
             </div>
             <div class="mb-3">
                 <label for="object" class="form-label">Oggetto Email:</label>
-                <input type="text" class="form-control" id="object" v-model.trim="form.object"
-                    placeholder="Inserisci un oggetto ">
+                <input type="text" class="form-control" :class="{ 'is-invalid': errors.object }" id="object"
+                    v-model.trim="form.object" placeholder="Inserisci un oggetto ">
+                <div v-if="errors.object" class="invalid-feedback">
+                    {{ errors.object }}
+                </div>
             </div>
             <div class="mb-3">
                 <label for="content" class="form-label">Contenuto Email:</label>
-                <textarea class="form-control" id="content" rows="10" v-model="form.content"></textarea>
+                <textarea class="form-control" :class="{ 'is-invalid': errors.content }" id="content" rows="10"
+                    v-model="form.content"></textarea>
+                <div v-if="errors.content" class="invalid-feedback">
+                    {{ errors.content }}
+                </div>
             </div>
             <button class="btn btn-success">Invia</button>
         </form>

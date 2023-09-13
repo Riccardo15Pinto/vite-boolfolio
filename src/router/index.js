b7aea143672fdd0310999ed6899dbdc157e2c668
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-13 15:24:28 +0200

    rec new component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter , createWebHistory} from 'vue-router';
 
 import HomePage from '../pages/HomPage.vue';
 import DetailPage from '../pages/DetailPage.vue';
+import FormPage from '../pages/FormPage.vue';
 
 const router = createRouter({
     history:createWebHistory(),
@@ -9,6 +10,7 @@ const router = createRouter({
     routes:[
         { path: '/' , name: 'home' , component: HomePage},
         { path: '/projects/:id' , name: 'DetailPage' , component: DetailPage},
+        { path: '/form' , name: 'FormPage' , component: FormPage},
     ]
 });
 

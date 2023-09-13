0f981bcb7430c6dd17ea0d0e33ed44ad260de4cd
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-13 15:43:37 +0200

    add method sendform

--- src/pages/FormPage.vue
+++ src/pages/FormPage.vue
@@ -1,42 +1,53 @@
 <script>
 import axios from 'axios';
-const baseEndpoint = 'http://localhost:8000/api/projects/';
+const endpoint = 'http://127.0.0.1:8000/api/contacts';
+const formField = { email: null, object: null, content: null };
 export default {
     data() {
         return {
-            project: null,
+            form: { ...formField },
         }
     },
     methods: {
 
-        addcontact() {
+        sendForm() {
 
-            const endpoint = baseEndpoint + this.$route.params.id;
-            axios.get(endpoint).then(res => {
-                this.project = res.data;
-            })
+
+            axios.post(endpoint, this.form)
+                .then(() => {
+                    this.form = formField;
+                })
+                .catch(err => { console.error(err) })
+                .then(() => { })
         },
 
     },
+
+    created() {
+        console.log(this.form);
+    }
 }
 
 </script>
 
 <template>
     <div class="container">
-        <form>
+        <form @submit.prevent="sendForm">
             <div class="mb-3">
                 <label for="email" class="form-label">Email:</label>
-                <input type="email" class="form-control" id="email" placeholder="Inserisci l'email">
+                <input type="email" class="form-control" id="email" v-model.trim="form.email"
+                    placeholder="Inserisci l'email">
             </div>
             <div class="mb-3">
                 <label for="object" class="form-label">Oggetto Email:</label>
-                <input type="text" class="form-control" id="object" placeholder="Inserisci un oggetto ">
+                <input type="text" class="form-control" id="object" v-model.trim="form.object"
+                    placeholder="Inserisci un oggetto ">
             </div>
             <div class="mb-3">
                 <label for="content" class="form-label">Contenuto Email:</label>
-                <textarea class="form-control" id="content" rows="10"></textarea>
+                <textarea class="form-control" id="content" rows="10" v-model="form.content"></textarea>
             </div>
+            <button class="btn btn-success">Invia</button>
         </form>
     </div>
 </template>
